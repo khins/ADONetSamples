749ba51372227047d7b1482ebed88c156b482361
khins <kevin@hinshawfam.com>
2019-09-16 16:19:52 -0500

    test

--- ADONET-Samples/ViewModels/DataTableViewModel.cs
+++ ADONET-Samples/ViewModels/DataTableViewModel.cs
@@ -125,7 +125,8 @@ namespace ADONET_Samples.ViewModels
       ResultText = string.Empty;
       RowsAffected = 0;
 
-      // Initialize DataTable object to null in case of an error.
+      // Initialize DataTable object to null in case of an error. 
+      // additional text
       DataTable dt = null;
 
       // Create SQL statement to submit
