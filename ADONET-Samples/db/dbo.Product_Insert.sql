153e448c9d73fe10b2862e116ed768f1c00b4de9
khins <kevin@hinshawfam.com>
2019-09-16 16:02:40 -0500

    Add project files.

---
+++ ADONET-Samples/db/dbo.Product_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Product_Insert]
+	@ProductName varchar(50),
+	@IntroductionDate datetime,
+	@Url varchar(100),
+	@Price decimal
+AS
+
+	INSERT INTO Product
+                         (ProductName, IntroductionDate, Url, Price)
+VALUES        (@ProductName,@IntroductionDate,@Url,@Price)
